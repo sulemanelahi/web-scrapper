7c1a1d503fe68d84ba20694d7c4bc8026af14014
Suleman Elahi <sulemane010@gmail.com>
2025-06-27 15:12:43 +0500

    ♻️ ref: Update the helper and function by converting into promises

--- app.js
+++ app.js
@@ -1,48 +1,25 @@
 const http = require("node:http");
 const url = require("node:url");
-const { getTitle } = require("./utils/helperFunctions");
+const { getTitle, generateListOfTitlesMarkup } = require("./utils/helperFunctions");
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
     const addresses = [];
     const sites = {};
 
     const parsedUrl = url.parse(req.url, true);
 
-    let completed = 0;
-
     if (typeof parsedUrl.query.address === "string") addresses.push(parsedUrl.query.address);
     if (Array.isArray(parsedUrl.query.address)) addresses.push(...parsedUrl.query.address);
 
     if (parsedUrl.pathname === "/I/want/title") {
-        addresses.forEach((address) => {
-            getTitle(address, (err, title) => {
-                sites[address] = title;
-                completed++;
+        for (const address of addresses) {
+            const title = await getTitle(address);
 
-                if (completed === addresses.length) {
-                    const listItems = [];
+            sites[address] = title;
+        }
 
-                    for (const address in sites) {
-                        const title = sites[address];
-
-                        listItems.push(`<li>${address} - "${title}"</li>`);
-                    }
-
-                    const html = `
-                    <html>
-                        <head></head>
-                        <body>
-                            <h1> Following are the titles of given websites: </h1>
-                            <ul>${listItems.join("\n")}</ul>
-                        </body>
-                    </html>
-                    `;
-
-                    res.writeHead(200, { "Content-Type": "text/html" });
-                    res.end(html);
-                }
-            });
-        });
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(generateListOfTitlesMarkup(sites));
     } else {
         res.writeHead(404);
         res.end("404 Not Found");
--- utils/helperFunctions.js
+++ utils/helperFunctions.js
@@ -1,29 +1,53 @@
 const https = require("node:https");
 const http = require("node:http");
 
-module.exports.getTitle = (siteUrl, callback) => {
-    let fullUrl = siteUrl.startsWith("http") ? siteUrl : `https://${siteUrl}`;
+module.exports.getTitle = async (siteUrl, callback) => {
+    return new Promise((resolve, rejects) => {
+        let fullUrl = siteUrl.startsWith("http") ? siteUrl : `https://${siteUrl}`;
 
-    if (fullUrl.split(".").length < 3 && !fullUrl.includes("www.")) fullUrl = fullUrl.replace("://", "://www.");
+        if (fullUrl.split(".").length < 3 && !fullUrl.includes("www.")) fullUrl = fullUrl.replace("://", "://www.");
 
-    const lib = fullUrl.startsWith("https") ? https : http;
+        const lib = fullUrl.startsWith("https") ? https : http;
 
-    try {
-        lib.get(fullUrl, (res) => {
-            let body = "";
+        try {
+            lib.get(fullUrl, (res) => {
+                let body = "";
 
-            res.on("data", (chunk) => (body += chunk));
-            res.on("end", () => {
-                const match = body.match(/<title>([^<]*)<\/title>/i);
+                res.on("data", (chunk) => (body += chunk));
+                res.on("end", () => {
+                    const match = body.match(/<title>([^<]*)<\/title>/i);
 
-                if (match && match[1]) {
-                    callback(null, match[1]);
-                } else {
-                    callback(null, "NO RESPONSE");
-                }
-            });
-        }).on("error", () => callback(null, "NO RESPONSE"));
-    } catch (err) {
-        callback(err, "NO RESPONSE");
+                    if (match && match[1]) {
+                        resolve(match[1]);
+                    } else {
+                        resolve("NO RESPONSE");
+                    }
+                });
+            }).on("error", () => rejects("NO RESPONSE"));
+        } catch (err) {
+            rejects("NO RESPONSE");
+        }
+    });
+};
+
+module.exports.generateListOfTitlesMarkup = (sites) => {
+    const listItems = [];
+
+    for (const address in sites) {
+        const title = sites[address];
+
+        listItems.push(`<li>${address} - "${title}"</li>`);
     }
+
+    const html = `
+                    <html>
+                        <head></head>
+                        <body>
+                            <h1> Following are the titles of given websites: </h1>
+                            <ul>${listItems.join("\n")}</ul>
+                        </body>
+                    </html>
+                    `;
+
+    return html;
 };
