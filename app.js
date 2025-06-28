03862608f4fa02f7abd69261fe2dd8c2fa84fab0
Suleman Elahi <sulemane010@gmail.com>
2025-06-28 08:51:20 +0500

    📦 misc: Update the log of server listening

--- app.js
+++ app.js
@@ -51,4 +51,4 @@ const server = http.createServer((req, res) => {
     }
 });
 
-server.listen(3000, () => console.log("Callback version running on 3000"));
+server.listen(3000, () => console.log("Async.js Callback version running on 3000"));
